e076915715c2e6c4fb1b5f79929ccd3d3f5532ff
dypool01 <dypool01@wsc.edu>
2023-06-06 14:43:44 -0500

    Replaced the data table with a model and a list to iterate through, similar to what was done on the react version of the personal site.

---
+++ Models/ProjectModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonalSiteFixed.Models
+{
+    public class ProjectModel
+    {
+        public string Name { get; set; }
+        public string WrittenLanguage { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string PathToPage { get; set; }
+    }
+}
